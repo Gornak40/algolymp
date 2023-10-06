d58c3d50306f5ca598f8ae4000db62c060e52257
Alexander Gornak <s-kozelsk@yandex.ru>
2023-10-06 16:58:54 +0300

    feat: add ejudge login, check, logout with session (#4)
    * feat: add ejudge login, check, logout with session
    
    * feat: -v and cid flag support
    
    * feat: config flag

--- cmd/ejik/main.go
+++ cmd/ejik/main.go
@@ -1,5 +1,39 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"strconv"
 
+	"github.com/Gornak40/algolymp/config"
+	"github.com/Gornak40/algolymp/ejudge"
+	"github.com/sirupsen/logrus"
+)
+
+func main() {
+	verboseFlag := flag.Bool("v", false, "show full output of check contest settings")
+	configFlag := flag.String("c", "config/config.json", "json config path")
+	flag.Parse()
+	if flag.NArg() == 0 {
+		logrus.Fatal("position argument cid required")
+	}
+	cid, err := strconv.Atoi(flag.Arg(0))
+	if err != nil {
+		logrus.WithField("cid", cid).Fatal("cid should be int")
+	}
+
+	cfg := config.NewConfig(*configFlag)
+	ejClient := ejudge.NewEjudge(&cfg.Ejudge)
+
+	sid, err := ejClient.Login()
+	if err != nil {
+		logrus.WithError(err).Fatal("login failed")
+	}
+
+	if err := ejClient.CheckContest(sid, cid, *verboseFlag); err != nil {
+		logrus.WithError(err).Fatal("check failed")
+	}
+
+	if err := ejClient.Logout(sid); err != nil {
+		logrus.WithError(err).Fatal("logout failed")
+	}
 }
--- config/config.go
+++ config/config.go
@@ -1,7 +1,25 @@
 package config
 
-type Ejudge struct {
-	URL      string `yaml:"url"`
-	Login    string `yaml:"login"`
-	Password string `yaml:"password"`
+import (
+	"encoding/json"
+	"os"
+
+	"github.com/Gornak40/algolymp/ejudge"
+	"github.com/sirupsen/logrus"
+)
+
+type Config struct {
+	Ejudge ejudge.Config `json:"ejudge"`
+}
+
+func NewConfig(path string) *Config {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		logrus.WithError(err).Fatal("failed to read config")
+	}
+	var cfg Config
+	if err := json.Unmarshal(data, &cfg); err != nil {
+		logrus.WithError(err).Fatal("failed to unmarshal config")
+	}
+	return &cfg
 }
