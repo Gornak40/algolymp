738d0d81d31c01d48580176b90998d35b607f4de
Gornak40 <s-kozelsk@yandex.ru>
2024-02-21 12:44:09 +0300

    feat(boban): boban ejudge runs filter + more linters small refactor

--- cmd/boban/main.go
+++ cmd/boban/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/Gornak40/algolymp/config"
@@ -9,15 +10,24 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+const (
+	DefaultRunsCount = 20
+)
+
 func main() {
-	parser := argparse.NewParser("ejik", "Refresh Ejudge contest by id.")
+	parser := argparse.NewParser("boban", "Filter Ejudge runs.")
 	cID := parser.Int("i", "cid", &argparse.Options{
 		Required: true,
 		Help:     "Ejudge contest ID",
 	})
-	verbose := parser.Flag("v", "verbose", &argparse.Options{
+	filter := parser.String("f", "filter", &argparse.Options{
 		Required: false,
-		Help:     "Show full output of check contest settings",
+		Help:     "Filter expression",
+	})
+	count := parser.Int("c", "count", &argparse.Options{
+		Required: false,
+		Help:     "Last runs count",
+		Default:  DefaultRunsCount,
 	})
 	if err := parser.Parse(os.Args); err != nil {
 		logrus.WithError(err).Fatal("bad arguments")
@@ -31,25 +41,19 @@ func main() {
 		logrus.WithError(err).Fatal("login failed")
 	}
 
-	if err := ejClient.Lock(sid, *cID); err != nil {
-		logrus.WithError(err).Fatal("lock contest failed")
-	}
-
-	if err := ejClient.Commit(sid); err != nil {
-		logrus.WithError(err).Fatal("commit failed")
-	}
-
-	if err := ejClient.CheckContest(sid, *cID, *verbose); err != nil {
-		logrus.WithError(err).Fatal("check failed")
-	}
-
 	csid, err := ejClient.MasterLogin(sid, *cID)
 	if err != nil {
 		logrus.WithError(err).Fatal("master login failed")
 	}
-	if err := ejClient.ReloadConfig(csid); err != nil {
-		logrus.WithError(err).Fatal("reload config failed")
+
+	runs, err := ejClient.FilterRuns(csid, *filter, *count)
+	if err != nil {
+		logrus.WithError(err).Fatal("filter runs failed")
 	}
+	for _, run := range runs {
+		fmt.Println(run) //nolint:forbidigo // Basic functionality.
+	}
+	logrus.WithField("runs", len(runs)).Info("filter result")
 
 	if err := ejClient.Logout(sid); err != nil {
 		logrus.WithError(err).Fatal("logout failed")
--- ejudge/ejudge.go
+++ ejudge/ejudge.go
@@ -1,10 +1,12 @@
 package ejudge
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 	"net/http/cookiejar"
 	"net/url"
+	"regexp"
 	"strconv"
 
 	"github.com/PuerkitoBio/goquery"
@@ -32,6 +34,7 @@ type Ejudge struct {
 func NewEjudge(cfg *Config) *Ejudge {
 	logrus.WithField("url", cfg.URL).Info("init ejudge engine")
 	jar, _ := cookiejar.New(nil)
+
 	return &Ejudge{
 		cfg: cfg,
 		client: &http.Client{
@@ -58,6 +61,7 @@ func (ej *Ejudge) postRequest(method string, params url.Values) (*http.Request, 
 	if err != nil {
 		return nil, nil, err
 	}
+
 	return resp.Request, doc, nil
 }
 
@@ -74,6 +78,7 @@ func (ej *Ejudge) Login() (string, error) {
 		return BadSID, err
 	}
 	logrus.WithField("SID", sid).Info("success login")
+
 	return sid, nil
 }
 
@@ -87,16 +92,18 @@ func (ej *Ejudge) Logout(sid string) error {
 		return err
 	}
 	logrus.WithField("SID", sid).Info("success logout")
+
 	return nil
 }
 
 func (ej *Ejudge) Lock(sid string, cid int) error {
 	logrus.WithFields(logrus.Fields{"CID": cid, "SID": sid}).Info("lock contest for editing")
 	_, _, err := ej.postRequest("serve-control", url.Values{
-		"contest_id": {strconv.FormatInt(int64(cid), 10)},
+		"contest_id": {strconv.Itoa(cid)},
 		"SID":        {sid},
 		"action":     {"276"},
 	})
+
 	return err
 }
 
@@ -111,13 +118,14 @@ func (ej *Ejudge) Commit(sid string) error {
 	}
 	status := doc.Find("h2").First().Text()
 	logrus.WithFields(logrus.Fields{"SID": sid}).Infof("ejudge answer %q", status)
+
 	return nil
 }
 
 func (ej *Ejudge) CheckContest(sid string, cid int, verbose bool) error {
 	logrus.WithFields(logrus.Fields{"CID": cid, "SID": sid}).Info("check contest settings, wait please")
 	_, doc, err := ej.postRequest("serve-control", url.Values{
-		"contest_id": {strconv.FormatInt(int64(cid), 10)},
+		"contest_id": {strconv.Itoa(cid)},
 		"SID":        {sid},
 		"action":     {"262"},
 	})
@@ -129,43 +137,82 @@ func (ej *Ejudge) CheckContest(sid string, cid int, verbose bool) error {
 	}
 	status := doc.Find("h2").First().Text()
 	logrus.WithFields(logrus.Fields{"CID": cid, "SID": sid}).Infof("ejudge answer %q", status)
+
 	return nil
 }
 
-func (ej *Ejudge) ReloadConfig(sid string, cid int) error {
+func (ej *Ejudge) MasterLogin(sid string, cid int) (string, error) {
 	req, _, err := ej.postRequest("new-master", url.Values{
-		"contest_id": {strconv.FormatInt(int64(cid), 10)},
+		"contest_id": {strconv.Itoa(cid)},
 		"SID":        {sid},
 		"action":     {"3"},
 	})
 	if err != nil {
-		return err
+		return "", err
 	}
 	csid := req.URL.Query().Get("SID")
 	if csid == "" {
-		return ErrParseMasterSID
+		return "", ErrParseMasterSID
 	}
 	logrus.WithFields(logrus.Fields{"CID": cid, "CSID": csid, "SID": sid}).Info("success master login")
-	_, _, err = ej.postRequest("new-master", url.Values{
+
+	return csid, nil
+}
+
+func (ej *Ejudge) FilterRuns(csid string, filter string, count int) ([]int, error) {
+	_, doc, err := ej.postRequest("new-master", url.Values{
+		"SID":              {csid},
+		"filter_view":      {"1"},
+		"filter_expr":      {filter},
+		"filter_first_run": {"-1"},
+		"filter_last_run":  {strconv.Itoa(-count)},
+	})
+	if err != nil {
+		return nil, err
+	}
+	ejErr := doc.Find("#container > pre")
+	if ejErr.Text() != "" {
+		return nil, errors.New(ejErr.Text())
+	}
+	res := doc.Find("#container > table:nth-child(18) > tbody > tr > td:nth-child(1)")
+	digits := regexp.MustCompile("[^0-9]+")
+	runsStr := res.Map(func(_ int, s *goquery.Selection) string {
+		return digits.ReplaceAllString(s.Text(), "")
+	})
+	runs := make([]int, 0, len(runsStr))
+	for _, s := range runsStr {
+		run, err := strconv.Atoi(s)
+		if err != nil {
+			return nil, err
+		}
+		runs = append(runs, run)
+	}
+
+	return runs, nil
+}
+
+func (ej *Ejudge) ReloadConfig(csid string) error {
+	_, _, err := ej.postRequest("new-master", url.Values{
 		"SID":    {csid},
 		"action": {"62"},
 	})
 	if err != nil {
 		return err
 	}
-	logrus.WithFields(logrus.Fields{"CID": cid, "CSID": csid, "SID": sid}).Info("success reload config")
+	logrus.WithFields(logrus.Fields{"CSID": csid}).Info("success reload config")
+
 	return nil
 }
 
 func (ej *Ejudge) CreateContest(sid string, cid int, tid int) error {
 	logrus.WithFields(logrus.Fields{"CID": cid, "TID": tid, "SID": sid}).Info("create contest")
 	_, doc, err := ej.postRequest("serve-control", url.Values{
-		"contest_id": {strconv.FormatInt(int64(cid), 10)},
+		"contest_id": {strconv.Itoa(cid)},
 		"SID":        {sid},
 		"num_mode":   {"1"},
 		"action":     {"259"},
 		"templ_mode": {"1"},
-		"templ_id":   {strconv.FormatInt(int64(tid), 10)},
+		"templ_id":   {strconv.Itoa(tid)},
 	})
 	if err != nil {
 		return err
@@ -175,25 +222,28 @@ func (ej *Ejudge) CreateContest(sid string, cid int, tid int) error {
 		status = "OK"
 	}
 	logrus.WithFields(logrus.Fields{"CID": cid, "TID": tid, "SID": sid}).Infof("ejudge answer %q", status)
+
 	return nil
 }
 
 func (ej *Ejudge) MakeInvisible(sid string, cid int) error {
 	logrus.WithFields(logrus.Fields{"CID": cid, "SID": sid}).Info("make invisible")
 	_, _, err := ej.postRequest("serve-control", url.Values{
-		"contest_id": {strconv.FormatInt(int64(cid), 10)},
+		"contest_id": {strconv.Itoa(cid)},
 		"SID":        {sid},
 		"action":     {"6"},
 	})
+
 	return err
 }
 
 func (ej *Ejudge) MakeVisible(sid string, cid int) error {
 	logrus.WithFields(logrus.Fields{"CID": cid, "SID": sid}).Info("make visible")
 	_, _, err := ej.postRequest("serve-control", url.Values{
-		"contest_id": {strconv.FormatInt(int64(cid), 10)},
+		"contest_id": {strconv.Itoa(cid)},
 		"SID":        {sid},
 		"action":     {"7"},
 	})
+
 	return err
 }
