748bfea6cd8b5a4b1b0ba9c7ea9a4e1544a6c1c3
Alexander Gornak <s-kozelsk@yandex.ru>
2024-02-21 13:44:41 +0300

    ci: semantic versioning bash script (#18)

---
+++ scripts/semver.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/bash
+major=0
+minor=0
+patch=0
+
+commits=$(git log --reverse --pretty=format:"%s")
+
+while read -r commit_msg; do
+    if [[ $commit_msg == major* ]]; then
+        ((major++))
+        minor=0
+        patch=0
+    elif [[ $commit_msg == feat* ]]; then
+        ((minor++))
+        patch=0
+    else
+        ((patch++))
+    fi
+    >&2 echo "[$major.$minor.$patch] $commit_msg"
+done <<< $commits
+
+echo "$major.$minor.$patch"
