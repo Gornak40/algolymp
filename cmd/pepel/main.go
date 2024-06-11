717caa5d61c4fafa4945002a4154893388c3434f
Alexander Gornak <s-kozelsk@yandex.ru>
2024-06-11 09:05:58 +0300

    feat: add pepel hasher solution generator (#30)
    * feat: add pepel hasher solution generator
    
    * docs: docs for pepel
    
    * docs: more pepel docs; pepel logo

---
+++ cmd/pepel/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/Gornak40/algolymp/pepel"
+	"github.com/akamensky/argparse"
+	"github.com/sirupsen/logrus"
+)
+
+func main() {
+	parser := argparse.NewParser("pepel", "Generate hasher solution based on a/ans/out files.")
+	infGlob := parser.String("i", "input", &argparse.Options{
+		Required: true,
+		Help:     "Input files glob",
+	})
+	ansGlob := parser.String("a", "answer", &argparse.Options{
+		Required: true,
+		Help:     "Answer files glob",
+	})
+	if err := parser.Parse(os.Args); err != nil {
+		logrus.WithError(err).Fatal("bad arguments")
+	}
+
+	infAll, err := filepath.Glob(*infGlob)
+	if err != nil {
+		logrus.WithError(err).Fatal("failed to match inf glob")
+	}
+	ansAll, err := filepath.Glob(*ansGlob)
+	if err != nil {
+		logrus.WithError(err).Fatal("failed to match ans glob")
+	}
+	if len(infAll) != len(ansAll) {
+		logrus.Fatal("input and answer files count mismatch")
+	}
+
+	program := []string{
+		"from hashlib import sha256",
+		"from sys import stdin, stdout",
+		"from base64 import b64decode",
+		"m = sha256()",
+		"m.update(stdin.buffer.read())",
+		"d = {",
+	}
+	for i, inf := range infAll {
+		ans := ansAll[i]
+		logrus.WithFields(logrus.Fields{
+			"inf": filepath.Base(inf), "ans": filepath.Base(ans),
+		}).Info("calculate hash")
+
+		mPair, err := pepel.GenMagicPair(inf, ans)
+		if err != nil {
+			logrus.WithError(err).Fatal("failed to generate magic pair")
+		}
+		program = append(program, fmt.Sprintf("\t%q: %q,", mPair.InfSHA256, mPair.AnsBase64))
+	}
+	program = append(program,
+		"}",
+		"stdout.buffer.write(b64decode(d[m.hexdigest()]))",
+	)
+
+	fmt.Println(strings.Join(program, "\n")) //nolint:forbidigo // Basic functionality.
+}
