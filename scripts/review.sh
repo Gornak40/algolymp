93d5e7cfed04629aca659c50e77a0149003e2883
Khetag Dzestelov <k.dzestelov@vk.team>
2024-08-16 00:16:35 +0300

    add run download

--- scripts/review.sh
+++ scripts/review.sh
@@ -8,10 +8,7 @@ NC='\033[0m'       # Text Reset
 Red='\033[0;31m'          # Red
 Green='\033[0;32m'        # Green
 IYellow='\033[0;93m'
-Blue='\033[0;34m'         # Blue
-Purple='\033[0;35m'       # Purple
 Cyan='\033[0;36m'         # Cyan
-White='\033[0;37m'        # White
 
 if [ "$#" -lt 3 ]; then
     echo "Usage: $0 <contestId> <filter> <count> [<0/1 disable PR filter>]"
@@ -21,8 +18,6 @@ fi
 contestId=$1
 if [ $4 == '1' ]; then
   filter=$2
-  RED='\033[0;31m'
-  NC='\033[0m' # No Color
   echo -e "${Red}WARNING${NC}    Pending review filter is disabled, don't OK the run uncontrollably!"
 else
   filter="($2)&&status==PR"
@@ -53,7 +48,7 @@ for file in "$contestId"/*; do
         runLinesCount=$(cat "$current" | awk '/=============== COMMENTS/{print NR-2; found=1; exit} END{if(!found) print NR}') # количестов строк кода в посылке до секции комментов
 
         while [ 1 ]; do
-            read -r -p "$(echo -e "${Green}INTERACT${NC}")   Type the resolution ($(echo -e "${Green}OK${NC}/${RED}RJ${NC}/rejudge")): " verdict
+            read -r -p "$(echo -e "${Green}INTERACT${NC}")   Type the resolution ($(echo -e "${Green}OK(щл)${NC}/${Red}RJ(ко)${NC}/skip/rejudge")): " verdict
             verdict=$(echo "$verdict" | tr '[:lower:]' '[:upper:]') # Преобразование в верхний регистр
 
             comment=$(head -n "$runLinesCount" "$current" | nl -w4 -s"] " | sed -e 's/ \([0-9]\)/[\1/' | grep -C 1 "///")
@@ -72,10 +67,12 @@ for file in "$contestId"/*; do
             if [ "$verdict" = "OK" ] || [ "$verdict" = "RJ" ]; then
                 echo $runId | ripper -i "$contestId" -s "$verdict" -c "$comment"
                 break
-             elif [ "$verdict" = "REJUDGE" ]; then
-                 echo $runId | ripper -i "$contestId" -s "rejudge"
-                 break
-             elif [ -z "$verdict" ]; then
+            elif [ "$verdict" = "REJUDGE" ]; then
+                echo $runId | ripper -i "$contestId" -s "rejudge"
+                break
+            elif [ "$verdict" == "SKIP" ]; then
+                break
+            elif [ -z "$verdict" ]; then
                 cleanup
                 exit 0
             fi
