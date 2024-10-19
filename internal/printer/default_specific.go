91ea0e80a957a2475bc049858dc0c83bd0b9570f
Gornak40 <s-kozelsk@yandex.ru>
2024-10-19 10:40:42 +0300

    chore: filepath base

--- internal/printer/default_specific.go
+++ internal/printer/default_specific.go
@@ -3,11 +3,13 @@
 package printer
 
 import (
+	"path/filepath"
+
 	"github.com/sirupsen/logrus"
 )
 
 func PrintFile(name, device string) error {
-	logrus.WithFields(logrus.Fields{"file": name, "printer": device}).Info("print file")
+	logrus.WithFields(logrus.Fields{"file": filepath.Base(name), "printer": device}).Info("print file")
 
 	return nil
 }
--- internal/printer/windows_specific.go
+++ internal/printer/windows_specific.go
@@ -5,12 +5,13 @@ package printer
 import (
 	"fmt"
 	"os/exec"
+	"path/filepath"
 
 	"github.com/sirupsen/logrus"
 )
 
 func PrintFile(name, device string) error {
-	logrus.WithFields(logrus.Fields{"file": name, "printer": device}).Info("print file")
+	logrus.WithFields(logrus.Fields{"file": filepath.Base(name), "printer": device}).Info("print file")
 
 	command := fmt.Sprintf("Get-Content -Path %q | Out-Printer -Name %q", name, device)
 	cmd := exec.Command("powershell", "-Command", command)
