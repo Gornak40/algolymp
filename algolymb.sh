7cdb2515cbc54a1b61f1ad20716d5cff7fa7eb8e
Gornak40 <s-kozelsk@yandex.ru>
2025-07-31 23:37:41 +0300

    feat: support @ in params

--- algolymb.sh
+++ algolymb.sh
@@ -13,16 +13,23 @@ pcurl() {
 	local method="$1"
 	shift
 
-	local params=("apiKey=${POLYGON_API_KEY}" "time=$(date +%s)" "$@")
+	local curl_params=("apiKey=${POLYGON_API_KEY}" "time=$(date +%s)" "$@")
+	local sig_params=()
+	for param in "${curl_params[@]}"; do
+		local key val
+		IFS='=' read -r key val <<< "${param}"
+		[[ "${val}" == @* ]] && val=$(<"${val#@}")
+		sig_params+=("${key}=${val}")
+	done
 
-	local sig_rand=$(head /dev/urandom | tr -dc '[:alnum:]' | head -c 6)
-	local sig_params=$(printf "%s\n" "${params[@]}" | sort | paste -sd '&')
-	local sig_data="${sig_rand}/${method}?${sig_params}#${POLYGON_SECRET}"
-	local sig_hash=$(echo -n "$sig_data" | sha512sum | awk '{print $1}')
+	local sig_rand=$(head /dev/urandom | LC_ALL=C tr -dc '[:alnum:]' | head -c 6)
+	local sig_string=$(printf "%s\n" "${sig_params[@]}" | sort | paste -sd '&')
+	local sig_data="${sig_rand}/${method}?${sig_string}#${POLYGON_SECRET}"
+	local sig_hash=$(echo -n "${sig_data}" | sha512sum | awk '{print $1}')
 
-	local api_sig="${sig_rand}${sig_hash}"
-
-	curl -fsSL "${POLYGON_URL}/${method}?${sig_params}&apiSig=${api_sig}"
+	curl_params+=("apiSig=${sig_rand}${sig_hash}")
+	curl -fsSL "${curl_params[@]/#/-F}" "${POLYGON_URL}/${method}"
 }
 
-pcurl problem.info problemId=436963
+echo -n 436963 > /tmp/test.txt
+pcurl problem.info problemId=@/tmp/test.txt
