59cafc964d50e757b5bf31a1d521be45947c2c02
Gornak40 <s-kozelsk@yandex.ru>
2025-08-07 23:29:47 +0300

    fix: replace exit with return

--- algolymb.sh
+++ algolymb.sh
@@ -1,24 +1,20 @@
 #!/bin/bash
 
-# It\'s just an experiment. Don\'t worry :)
-
-set -euo pipefail
-
-# TODO: remove.
-# set -x
-
-################################################################################
-
-readonly POLYGON_URL="https://polygon.codeforces.com/api"
+: ${POLYGON_URL:="https://polygon.codeforces.com"}
 
 pcurl() {
+	[[ $# -eq 0 ]] && echo "Usage: $0 METHOD [PARAM=VALUE]... [PARAM=@FILE]..." >&2 && return 2
+
+	: ${POLYGON_API_KEY:?"env required"}
+	: ${POLYGON_SECRET:?"env required"}
+
 	local method="$1"
 	shift
 
 	local curl_params=("apiKey=${POLYGON_API_KEY}" "time=$(date +%s)" "$@")
 	local sig_params=()
 	for param in "${curl_params[@]}"; do
-		local key val
+		local key= val=
 		IFS='=' read -r key val <<< "${param}"
 		[[ "${val}" == @* ]] && val=$(<"${val#@}")
 		sig_params+=("${key}=${val}")
@@ -30,31 +26,20 @@ pcurl() {
 	local sig_hash=$(echo -n "${sig_data}" | sha512sum | awk '{print $1}')
 
 	curl_params+=("apiSig=${sig_rand}${sig_hash}")
-	local response=$(curl -sSL -w "\n%{http_code}" "${curl_params[@]/#/-F}" "${POLYGON_URL}/${method}")
+	local response=$(curl -sSL -w "\n%{http_code}" "${curl_params[@]/#/-F}" "${POLYGON_URL}/api/${method}")
 
 	local http_code=$(tail -n 1 <<< "${response}")
 	local body=$(head -n -1 <<< "${response}")
 	case "${http_code}" in
 		200)
-			jq -r ".result" <<< "${body}" ;;
+			jq -r ".result // .status" <<< "${body}" ;;
 		400)
-			jq -r ".comment" <<< "${body}"; exit 1 ;;
+			jq -r ".comment" <<< "${body}"; return 1 ;;
 		*)
-			printf "%s" "${body}"; exit 2 ;;
+			printf "%s" "${body}"; return 2 ;;
 	esac
 }
-
-################################################################################
 
 algp() {
 	pcurl "problem.$1" problemId="$2" "${@:3}"
 }
-
-################################################################################
-
-echo -n 436963 > /tmp/test.txt
-algp info @/tmp/test.txt
-
-algp statements 436963
-
-algp validator 436963
